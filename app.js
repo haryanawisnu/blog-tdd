3732b5ec3ee94fc946dce066f51a594e4d4300c9
haryanawisnu <arya.wisnu05@gmail.com>
2017-05-09 19:32:55 +0700

    add vue

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 var blog = require('./routes/blog');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 var db_config = {
   development: "mongodb://localhost/blog",
@@ -14,10 +15,23 @@ mongoose.connect(db_config[app_env], function(err, res) {
   console.log('Connect to database : ' + db_config[app_env]);
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "DELETE")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token")
+  next()
+});
+
+
+// app.use(cors())
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+//Corse
+
 
 app.use('/blog', blog);
 
